9591dc9a09e2f4c8a2c32c39571d2b3ec2d8fb08
Mourey jerome <jerome@everycheck.fr>
2019-01-24 14:55:24 +0100

    initial commit

---
+++ ResponseBuilder.php
@@ -0,0 +1,109 @@
+<?php
+namespace CoreBundle\ControllerTrait;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Form;
+use CoreBundle\Entity\ComponentFile;
+
+class ResponseBuilder 
+{
+    protected $serializer;
+
+    public function __construct($serialize)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function json($entity,$code) : Response
+    {
+        $data = $this->serializer->serialize($entity,'json');
+        return new Response($data,$code,['Content-type'=>'application/json']);
+    }
+
+    public function empty() : Response
+    {
+        return new Response('',Response::HTTP_NO_CONTENT);
+    }
+
+    public function notFound() : Response
+    {
+        return $this->json(['message'=>'Entity not found'],Response::HTTP_NOT_FOUND);
+    }
+
+    public function ok($entity) : Response
+    {
+        return $this->json($entity,Response::HTTP_OK);
+    }
+
+    public function created($entity) : Response
+    {
+        return $this->json($entity,Response::HTTP_CREATED);
+    }
+
+    public function deleted() : Response
+    {
+        return $this->json(['message'=> 'No content'],Response::HTTP_NO_CONTENT);
+    }
+
+    public function conflict($entity) : Response
+    {
+        return $this->json($entity,Response::HTTP_CONFLICT);
+    }
+
+    public function forbiddenRoute() : Response
+    {
+        return $this->forbidden('cannot access this route');
+    }
+
+    public function forbiddenAcl() : Response
+    {
+        return $this->forbidden('cannot access not owned entity');
+    }
+
+    public function forbidden($reason) : Response
+    {
+        return $this->json(['message'=>"Forbidden : $reason."],Response::HTTP_FORBIDDEN);
+    }
+
+    public function formError(\Symfony\Component\Form\Form $form) : Response
+    {
+        return $this->badRequest($this->getErrorMessages($form));
+    }
+
+    public function badRequest($entity) : Response
+    {
+        return $this->json($entity,Response::HTTP_BAD_REQUEST);
+    }
+
+    private function getErrorMessages(Form $form) 
+    {
+        $errors = $this->getAllErrors($form);
+        $this->getChildField($form,$errors);
+
+        if (empty($errors['children']))
+        {
+            $errors['children'] = null;
+        }
+        return $errors;
+    }
+
+    private function getChildField(Form $form,&$errors)
+    {
+        foreach ($form->all() as $child) 
+        {
+            $errors['children'][$child->getName()] = $this->getErrorMessages($child);
+        }
+    }
+
+    private function getAllErrors(Form $form)
+    {
+        $errors = array('children' => array());
+        foreach ($form->getErrors() as $key => $error) 
+        {
+                if(empty($errors['errors'])) $errors['errors'] = array();
+                array_push($errors['errors'], $error->getMessage());
+        }
+        return $errors;
+    }
+
+}
